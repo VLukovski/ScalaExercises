a31313b14b60d05fcfc22aa42be279ee321b65e0
VLukovski <valentinlukovski1@gmail.com>
2019-07-11 11:31:30 +0100

    functional 1

--- src/main/scala/sample/exercises3/Exercises.scala
+++ src/main/scala/sample/exercises3/Exercises.scala
@@ -4,7 +4,6 @@ import java.util.TimeZone
 
 object Exercises {
     def functional1(): Unit = {
-      println(TimeZone.getAvailableIDs.map(_.split("/").tail).filter(_.length > 0).map(_(0)).toList)
-      println(TimeZone.getAvailableIDs().toList)
+      println(TimeZone.getAvailableIDs.filter(_.contains("/")).map(_.split("/").tail).map(_(0)).toList)
     }
 }
